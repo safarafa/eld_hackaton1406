b64fcbdfcd5270e14e9c5d97a541c52f9fcb8de7
efielo <skowronskifilip@gmail.com>
2017-06-14 14:23:04 +0200

    Create Temperature.java

--- JavaServer/src/main/java/domain/Temperature.java
+++ JavaServer/src/main/java/domain/Temperature.java
@@ -1,32 +1,19 @@
 package domain;
 
 
+enum TempType{
+	CELSIUS,
+	FAHRENHEIT
+}
+
 public class Temperature {
 
     private static int tempValue;
-    private String tempType;
-    public static final int maxvalue=50;
-    public static final int minvalue =0;
+    public static final int MAXVALUE=50;
+    public static final int MINVALUE =0;
 
-    public Temperature(int value, String type) {
-        this.tempValue = value;
-        this.tempType = type;
-
-    }
-
-    public Temperature() {
-    }
-
-    public void increase(){
-
-    }
-
-    public static int getMinvalue() {
-        return minvalue;
-    }
-
-    public static int getMaxvalue() {
-        return maxvalue;
+    public Temperature(int value) {
+        tempValue = value;
     }
 
     public static int getValue() {
@@ -34,14 +21,6 @@ public class Temperature {
     }
 
     public static void setValue(int value) {
-        Temperature.tempValue = value;
-    }
-
-    public String getType() {
-        return tempType;
-    }
-
-    public void setType(String type) {
-        this.tempType = type;
+        Temperature.value = value;
     }
 }
