ff418e25df8aeb06d3a43614448143294f28685f
Michał <sawluk.michal@gmail.com>
2017-06-14 14:48:40 +0200

    final

---
+++ widok/js/tempReading.js
@@ -0,0 +1,52 @@
+setInterval(function(){
+    var http = new XMLHttpRequest();
+    var url ='http://localhost:8080/temperature';
+    var method = 'GET';
+    http.open(method,url);
+
+    http.onreadystatechange=function () {
+    if(http.readyState == XMLHttpRequest.DONE && http.status === 200){
+        var temperature=JSON.parse(http.responseText);
+        
+        document.getElementById('temperatura').value=temperature.tempValue+' '+temperature.tempType;
+    }else if(http.readyState === XMLHttpRequest.DONE){
+        console.log('error');
+    }
+}
+http.send();    
+},2000);
+
+var inc = document.getElementById('incr');
+
+inc.onclick = function (){
+    var http = new XMLHttpRequest();
+    var url ='temperature/increment';
+    var method = 'POST';
+    http.open(method,url,true);
+    http.onreadystatechange=function () {
+    if(http.readyState == XMLHttpRequest.DONE && http.status === 200){
+        window.alert('TempInceremented');
+    }else if(http.readyState === XMLHttpRequest.DONE){
+        console.log('error');
+    } 
+       
+    http.send();
+    }
+};
+
+var dec = document.getElementById('decr');
+    
+dec.onclick =function (){
+    var http = new XMLHttpRequest();
+    var url ='temperature/decrement';
+    var method = 'POST';
+    http.open(method,url,true);
+    http.onreadystatechange=function () {
+    if(http.readyState == XMLHttpRequest.DONE && http.status === 200){
+        alert('TempDeceremented');
+    }else if(http.readyState === XMLHttpRequest.DONE){
+        console.log('error');
+    }      
+    http.send();      
+    }
+};
