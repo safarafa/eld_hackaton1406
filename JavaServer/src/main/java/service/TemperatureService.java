99e3a8baab7e03564ccb8c912e9a19709a8cda2d
Mateusz <zar1117@o2.pl>
2017-06-14 13:29:36 +0200

    Java serwer

--- JavaServer/src/main/java/service/TemperatureService.java
+++ JavaServer/src/main/java/service/TemperatureService.java
@@ -2,18 +2,25 @@ package service;
 
 
 import domain.Temperature;
+import exceptions.InvalidTemperatureExceptions;
 
-public class TemperatureService {
-     void increment(){
-         if(Temperature.getValue()<=Temperature.maxvalue && Temperature.getValue()>Temperature.minvalue)
+public class TemperatureService{
+     public void increment()throws InvalidTemperatureExceptions{
+         if(Temperature.getValue()+1<=Temperature.maxvalue)
          {
-             Temperature.setValue(Temperature.getValue());
+             Temperature.setValue(Temperature.getValue()+1);
          }
+        else throw new InvalidTemperatureExceptions("Invalid temperature");
 
 
      }
 
-     void decrement(){
+     public void decrement() throws InvalidTemperatureExceptions{
+         if(Temperature.getValue()-1>=Temperature.minvalue)
+         {
+             Temperature.setValue(Temperature.getValue()-1);
+         }
+         else throw new InvalidTemperatureExceptions("Invalid temperature");
 
      }
 }
